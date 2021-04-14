d6b7c290d34e817a479b9c8c32e551adb40d8ab4
Cassandra <45634386+CassandraCH@users.noreply.github.com>
2021-04-14 20:45:59 +0200

    Implémentation reset + modif interface

--- src/jeuDeLaVie/JeuDeLaVie.java
+++ src/jeuDeLaVie/JeuDeLaVie.java
@@ -33,7 +33,6 @@ public class JeuDeLaVie implements Observable {
 
     private int numGeneration;
     private int nbCellulesVivantes;
-    private int vitesse;
 
     private List<Observateur> observateurs;
     private List<Commande> commandes;
@@ -43,120 +42,26 @@ public class JeuDeLaVie implements Observable {
     public JeuDeLaVie() {
         this.observateurs = new ArrayList<>();
         this.commandes = new ArrayList<>();
-        this.numGeneration = 0;
-        this.vitesse = 5;
     }
 
     public static void main(String[] args) {
-        JFrame fenetre = new JFrame("Le super jeu de la vie");
-
-        fenetre.setSize(new Dimension(670, 720));// regler la taille
-        fenetre.setLocationRelativeTo(null); // centrer la fenetre
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //gestion de la fermeture de la fenetre
-        fenetre.setVisible(true); // afficher
-
         JeuDeLaVie jeu = new JeuDeLaVie();
         jeu.setXMax(150);
         jeu.setYMax(150);
         jeu.initialiseGrille();
 
-        Visiteur vClassic = new VisiteurClassique(jeu);
-        Visiteur vHighLife = new VisiteurHighLife(jeu);
-        Visiteur vDayNight = new VisiteurDayAndNight(jeu);
-
-        jeu.setVisiteur(vClassic);
-
         ObservateurGeneration og = new ObservateurGeneration(jeu);
         jeu.attacheObservateur(og);
-
-        JPanel boxPrincipale = new JPanel();
-        boxPrincipale.setLayout(new BoxLayout(boxPrincipale, BoxLayout.Y_AXIS));
 
         JeuDeLaVieUI jdv = new JeuDeLaVieUI(jeu);
         jeu.attacheObservateur(jdv);
 
-        jdv.setPreferredSize(new Dimension(600,600));
-        jdv.setBounds(50,25,600,600);
-        boxPrincipale.add(jdv);
-
-        JPanel panneau = new JPanel();
-        panneau.setPreferredSize(new Dimension(700,100));
-
-        JButton btnLancer = new JButton("Lancer", new ImageIcon("./assets/play.png"));
-        JButton btnPause = new JButton("Pause", new ImageIcon("./assets/pause.png"));
-        JButton btnSuivant = new JButton("Suivant", new ImageIcon("./assets/suivant.png"));
-
-        btnLancer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Lancement cliqué");
-                jdv.lancer();
-            }
-        });
-
-        btnPause.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Pause cliqué");
-                jdv.arreter();
-            }
-        });
-
-        btnSuivant.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Suivant cliqué");
-                jeu.calculerGenerationSuivante();
-            }
-        });
-
-        panneau.add(btnLancer);
-        panneau.add(btnPause );
-        panneau.add(btnSuivant);
-
-        JPanel slider = new JPanel();
-        JSlider s = new JSlider(JSlider.HORIZONTAL,2,8,5);
-        s.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                jeu.vitesse = s.getValue();
-            }
-        });
-
-        slider.setLayout(new FlowLayout(FlowLayout.TRAILING, 15, 5));
-        slider.add(new JLabel("Vitesse"));
-        slider.add(s);
-        panneau.add(slider);
-
-        panneau.add(new JLabel("Choix des règles"));
-
-        JComboBox<String> choixRegles = new JComboBox<>();
-        choixRegles.addItem("Classique");
-        choixRegles.addItem("HighLife");
-        choixRegles.addItem("Day & Night");
-        choixRegles.setSelectedIndex(0);
-
-        choixRegles.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String choix = choixRegles.getSelectedItem().toString();
-                if(choix == "Classique"){ jeu.setVisiteur(vClassic); }
-                else if(choix == "HighLife"){ jeu.setVisiteur(vHighLife); }
-                else { jeu.setVisiteur(vDayNight); }
-                System.out.println("Regles : "+choix);
-            }
-        });
-
-        panneau.add(choixRegles);
-
-        boxPrincipale.add(panneau);
-
-        fenetre.add(boxPrincipale);
-        fenetre.pack();
+        jdv.creerInterface();
     }
 
     public void initialiseGrille() {
         this.grille = new Cellule[xMax][yMax];
+        this.numGeneration = 0;
         this.nbCellulesVivantes = 0;
 
         for(int x = 0; x < xMax-1; x++){
@@ -239,8 +144,6 @@ public class JeuDeLaVie implements Observable {
     public int getXMax() { return xMax; }
 
     public int getYMax() { return yMax; }
-
-    public int getVitesse() { return vitesse; }
 
     public int getNumGeneration() { return numGeneration; }
 }
--- src/jeuDeLaVie/observateurs/JeuDeLaVieUI.java
+++ src/jeuDeLaVie/observateurs/JeuDeLaVieUI.java
@@ -1,17 +1,30 @@
 package jeuDeLaVie.observateurs;
 
 import jeuDeLaVie.JeuDeLaVie;
+import jeuDeLaVie.visiteurs.Visiteur;
+import jeuDeLaVie.visiteurs.VisiteurClassique;
+import jeuDeLaVie.visiteurs.VisiteurDayAndNight;
+import jeuDeLaVie.visiteurs.VisiteurHighLife;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
 public class JeuDeLaVieUI extends JPanel implements Observateur, Runnable {
     private JeuDeLaVie jeu;
     private boolean enCours;
     private Thread thread;
+    private int vitesse;
+    private JTextArea infos;
 
     public JeuDeLaVieUI(JeuDeLaVie jeu) {
         this.jeu = jeu;
+        this.vitesse = 5;
     }
 
     public void actualise() {
@@ -21,6 +34,7 @@ public class JeuDeLaVieUI extends JPanel implements Observateur, Runnable {
     public void arreter() {
         this.enCours = false;
     }
+
     public void lancer() {
         this.enCours = true;
         thread = new Thread(this);
@@ -39,13 +53,149 @@ public class JeuDeLaVieUI extends JPanel implements Observateur, Runnable {
         }
     }
 
+    public void creerInterface(){
+        JFrame fenetre = new JFrame("Le super jeu de la vie");
+
+        fenetre.setSize(new Dimension(670, 720));// regler la taille
+        fenetre.setLocationRelativeTo(null); // centrer la fenetre
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //gestion de la fermeture de la fenetre
+        fenetre.setVisible(true); // afficher
+
+        JPanel boxPrincipale = new JPanel();
+        boxPrincipale.setBorder(new EmptyBorder(10,10,10,10));
+        boxPrincipale.setLayout(new BoxLayout(boxPrincipale, BoxLayout.X_AXIS));
+
+        this.setPreferredSize(new Dimension(600,600));
+        boxPrincipale.add(this);
+
+        JPanel panneau = new JPanel();
+        panneau.setLayout(new BorderLayout(5,25));
+        panneau.setPreferredSize(new Dimension(400,600));
+
+        JPanel haut = new JPanel();
+        haut.setLayout(new BorderLayout(5,25));
+
+
+        JPanel jp = new JPanel();
+        infos = new JTextArea(2,20);
+        infos.setEditable(false);
+        jp.setPreferredSize(new Dimension(300,50));
+        infos.setText("Génération numéro "+jeu.getNumGeneration()+"\nCellules vivantes : "+jeu.calculerNbCellulesVivantes());
+        jp.add(infos);
+
+        haut.add(jp);
+
+        JPanel boutons = new JPanel();
+        boutons.setLayout(new FlowLayout(FlowLayout.CENTER, 15, 5));
+
+        JButton btnLancer = new JButton("Lancer", new ImageIcon("./assets/play.png"));
+        JButton btnPause = new JButton("Pause", new ImageIcon("./assets/pause.png"));
+        JButton btnSuivant = new JButton("Suivant", new ImageIcon("./assets/suivant.png"));
+
+        btnLancer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { lancer();  }
+        });
+
+        btnPause.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { arreter(); }
+        });
+
+        btnSuivant.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { jeu.calculerGenerationSuivante(); }
+        });
+
+        boutons.add(btnLancer);
+        boutons.add(btnPause );
+        boutons.add(btnSuivant);
+
+        haut.add(boutons, BorderLayout.SOUTH);
+        panneau.add(haut, BorderLayout.NORTH);
+
+        JPanel options = new JPanel();
+
+        JSlider s = new JSlider(JSlider.HORIZONTAL,2,8,5);
+
+        Hashtable labels = new Hashtable();
+        labels.put( 2, new JLabel("Lent") );
+        labels.put( 8, new JLabel("Rapide") );
+
+        s.setLabelTable(labels);
+        s.setMinorTickSpacing(1);
+        s.setMajorTickSpacing(8);
+        s.setPaintTicks(true);
+        s.setPaintLabels(true);
+
+        s.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) { vitesse = s.getValue(); }
+        });
+
+        options.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 35));
+        options.add(new JLabel("Vitesse"));
+        options.add(s);
+
+
+        JPanel choixRegles = new JPanel();
+        choixRegles.setLayout(new FlowLayout(FlowLayout.TRAILING, 15, 5));
+        choixRegles.add(new JLabel("Choix des règles"));
+
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.addItem("Classique");
+        comboBox.addItem("HighLife");
+        comboBox.addItem("Day & Night");
+        comboBox.setSelectedIndex(0);
+
+
+        choixRegles.add(comboBox);
+
+        Visiteur vClassic = new VisiteurClassique(jeu);
+        Visiteur vHighLife = new VisiteurHighLife(jeu);
+        Visiteur vDayNight = new VisiteurDayAndNight(jeu);
+
+        jeu.setVisiteur(vClassic);
+
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String choix = comboBox.getSelectedItem().toString();
+                if(choix == "Classique"){ jeu.setVisiteur(vClassic); }
+                else if(choix == "HighLife"){ jeu.setVisiteur(vHighLife); }
+                else { jeu.setVisiteur(vDayNight); }
+            }
+        });
+
+        options.add(choixRegles);
+
+        JButton reset = new JButton("Réinitialiser", new ImageIcon("./assets/reset.png"));
+        reset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Reinitialiser");
+                jeu.initialiseGrille();
+            }
+        });
+
+        options.add(reset);
+
+        panneau.add(options, BorderLayout.CENTER);
+
+        boxPrincipale.add(panneau);
+
+        fenetre.add(boxPrincipale);
+        fenetre.pack();
+    }
+
     @Override
     public void run() {
         while(enCours){
+            updateAffichage();
             jeu.calculerGenerationSuivante();
 
             try {
-                int tmp = jeu.getVitesse();
+                int tmp = vitesse;
 
                 if(tmp == 5) tmp *= 100;
                 else if(tmp > 5) tmp = 1000 - (tmp*100);
@@ -56,5 +206,9 @@ public class JeuDeLaVieUI extends JPanel implements Observateur, Runnable {
                 System.out.println("Erreur : "+ex);
             }
         }
+    }
+
+    public void updateAffichage(){
+        this.infos.setText("Génération numéro "+jeu.getNumGeneration()+"\nCellules vivantes : "+jeu.calculerNbCellulesVivantes());
     }
 }
